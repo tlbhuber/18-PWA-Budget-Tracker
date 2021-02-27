6917020167c0227443520a4f9f8fabe860a8e9ed
Timothy Balascak-Huber <70402692+tlbhuber@users.noreply.github.com>
2021-02-27 17:34:08 -0500

    update server.js for deployment

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Comment this line when deploying to Heroku
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Budget", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Budget", { 
+  useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
+     });
 
 // routes
 app.use(require("./routes/api.js"));
