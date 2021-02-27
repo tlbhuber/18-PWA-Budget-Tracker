ef2a07b26f1c85780a0cf717a239914946003d40
Timothy Balascak-Huber <70402692+tlbhuber@users.noreply.github.com>
2021-02-27 17:43:41 -0500

    deployment ready

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -26,6 +26,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("App running on port 3000!");
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
 });
