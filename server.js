62c6fde8354b90ba0d55e27067a998f3fb2aadf6
Timothy Balascak-Huber <70402692+tlbhuber@users.noreply.github.com>
2021-02-27 17:08:18 -0500

    Updating Heroku/Mongo Connections for deployment

--- server.js
+++ server.js
@@ -15,10 +15,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// Comment this line when deploying to Heroku
+mongoose.connect("mongodb://localhost/Budget", {useNewUrlParser: true, useFindAndModify: false});
+
+// Comment this line out when pushing to gitHub (Uncomment when deploying to Heroku)
+    // mongoose.connect(
+    //   process.env.MONGODB_URI || 'mongodb://localhost/Budget',
+    //   {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    //     useCreateIndex: true,
+    //     useFindAndModify: false
+    //   }
+    // );
 
 // routes
 app.use(require("./routes/api.js"));
